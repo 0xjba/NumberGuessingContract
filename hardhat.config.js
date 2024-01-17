eb6ac1289153147977f9b933aa3414e63fd2bc14
Amir Hussain <amir.hussain@10pearls.com>
2024-01-17 21:37:21 +0500

    my commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,29 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("ten-hardhat-plugin")
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.23",
   networks: {
-   
+
     hardhat: {
       gas: 'auto',
       gasPrice: 'auto',
     },
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/01228a1a14684cad8ba90a89c7d08d96`,
-      accounts: ['4f81556f880c060b7863252ed423df9506f8a4887ef8f5edf89ecfd3ae34b748']
-    },
     obscuro: {
-      url: "https://testnet.ten.xyz/v1/?token=eb3a9365ab669f376269931140b6a4d9acaeca0b",
-      accounts: ["4f81556f880c060b7863252ed423df9506f8a4887ef8f5edf89ecfd3ae34b748"],
-      useGateway:false,
-    },
-    baseGoerli: {
-      url: "https://goerli.infura.io/v3/01228a1a14684cad8ba90a89c7d08d96",
+      url: "https://testnet.ten.xyz/v1/?token=cb96c063e0deeffab90050d67abbb5c8a1929201",
       accounts: ["4f81556f880c060b7863252ed423df9506f8a4887ef8f5edf89ecfd3ae34b748"],
     },
   },
   etherscan: {
-    apiKey: "D6A34JPYNRD15KSHGCUVPPIC1UWEJ8X4SI",
+    apiKey: "91815f76E9Eb2298d6b49d75b5f7EC80e4Ea1E4B",
   },
 };
