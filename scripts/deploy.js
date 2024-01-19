0bc308f934ae71bbc21db998714263d7643157e3
Jobin Babu Ayathil <jobinb6444@outlook.com>
2024-01-19 16:38:13 +0530

    Removed Redundant Code
    Since the stake amount varies by game level, a hardcoded GameedAmount is redundant. To display the stake amount, dynamically retrieve these values from the contract based on the selected level. This approach ensures accurate and updated information on the frontend without unnecessary hardcoding

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,16 +6,13 @@ async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unnumberGuessingGameTime = currentTimestampInSeconds + 60;
 
-  const numberGuessingGameedAmount = ethers.parseEther("0.001");
   const NumberGuessingGame = await ethers.getContractFactory("NumberGuessingGame");
   const numberGuessingGame = await NumberGuessingGame.deploy("0xC04Bc14B1AB0b5fd323CA3A7CdC59434999Dce3e", ownerAddress);
 
   await numberGuessingGame.waitForDeployment();
 
   console.log(
-    `NumberGuessingGame with ${ethers.utils.formatEther(
-      numberGuessingGameedAmount
-    )}ETH and unnumberGuessingGame timestamp ${unnumberGuessingGameTime} deployed to ${numberGuessingGame.target}`
+    `Number GuessingGame Deployed to ${numberGuessingGame.target} at ${unnumberGuessingGameTime}'
   );
 }
 
